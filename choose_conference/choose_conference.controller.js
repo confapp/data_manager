4daf4c02b23ae8d99b4978bbe903eb6a5cac6b58
Stephen Oney <swloney@gmail.com>
2016-01-03 00:45:39 -0500

    Added links to source pages

--- choose_conference/choose_conference.controller.js
+++ choose_conference/choose_conference.controller.js
@@ -5,27 +5,29 @@ function ChooseConferenceController($scope, $location, $rootScope, Authenticatio
 	var ref = APIServices.getFirebaseRef();
 	ref.onAuth(function() {
 		var authInfo = AuthenticationService.getAuthInformation();
-		var myConferencesRef = ref.child('admin_users').child(authInfo.uid).child('conferences');
-		AuthenticationService.isRootUser().then(function(isRoot) {
-			var conferencesRef = APIServices.getConferencesRef();
+		if(authInfo) {
+			var myConferencesRef = ref.child('admin_users').child(authInfo.uid).child('conferences');
+			AuthenticationService.isRootUser().then(function(isRoot) {
+				var conferencesRef = APIServices.getConferencesRef();
 
-			if(isRoot) {
-				$scope.myConferences = $firebaseObject(conferencesRef);
-			} else {
-				var authInfo = AuthenticationService.getAuthInformation();
-				var conferences = $firebaseObject(ref.child('admin_users').child(authInfo.uid).child('conferences'));
+				if(isRoot) {
+					$scope.myConferences = $firebaseObject(conferencesRef);
+				} else {
+					var authInfo = AuthenticationService.getAuthInformation();
+					var conferences = $firebaseObject(ref.child('admin_users').child(authInfo.uid).child('conferences'));
 
-				$scope.myConferences = {};
-				conferences.$watch(function() {
 					$scope.myConferences = {};
-					conferences.$loaded().then(function(value) {
-						angular.forEach(value, function(conf, key) {
-							$scope.myConferences[key] = $firebaseObject(conferencesRef.child(key));
+					conferences.$watch(function() {
+						$scope.myConferences = {};
+						conferences.$loaded().then(function(value) {
+							angular.forEach(value, function(conf, key) {
+								$scope.myConferences[key] = $firebaseObject(conferencesRef.child(key));
+							});
 						});
 					});
-				});
-			}
-		});
+				}
+			});
+		}
 		/*
 
 		var conferencesRef = APIServices.getConferencesRef();
