cf7a143e60ed1e172ff0248fe0d61cbc9846dc35
Steve Oney <swloney@gmail.com>
2016-06-06 11:02:40 -0400

    Working on reading google sheets

--- services/google_sheets.js
+++ services/google_sheets.js
@@ -1,3 +1,26 @@
+app.factory('GoogleSheetsService', GoogleSheetsService);
+
+GoogleSheetsService.$inject=['$q', '$rootScope', '$cookies', 'APIServices'];
+function GoogleSheetsService($q, $rootScope, $cookies, APIServices) {
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+}
 // Your Client ID can be retrieved from your project in the Google
 // Developer Console, https://console.developers.google.com
 var CLIENT_ID = '551899241610-ei30p9kfjj2sbi18rlrfl271vqa9h3f3.apps.googleusercontent.com';
@@ -5,43 +28,53 @@ var CLIENT_ID = '551899241610-ei30p9kfjj2sbi18rlrfl271vqa9h3f3.apps.googleuserco
 var SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
 
 /**
- * Check if current user has authorized this application.
- */
+* Check if current user has authorized this application.
+*/
 function checkAuth() {
-  gapi.auth.authorize(
-    {
-      'client_id': CLIENT_ID,
-      'scope': SCOPES.join(' '),
-      'immediate': true
+    gapi.auth.authorize({
+        'client_id': CLIENT_ID,
+        'scope': SCOPES.join(' '),
+        'immediate': true
     }, handleAuthResult);
 }
 
 /**
- * Handle response from authorization server.
- *
- * @param {Object} authResult Authorization result.
- */
+* Handle response from authorization server.
+*
+* @param {Object} authResult Authorization result.
+*/
 function handleAuthResult(authResult) {
-  var authorizeDiv = document.getElementById('authorize-div');
-  if (authResult && !authResult.error) {
-    // Hide auth UI, then load client library.
-    authorizeDiv.style.display = 'none';
-    loadSheetsApi();
-  } else {
-    // Show auth UI, allowing the user to initiate authorization by
-    // clicking authorize button.
-    authorizeDiv.style.display = 'inline';
-  }
+    console.log(authResult);
+    var authorizeDiv = document.getElementById('authorize-div');
+    if (authResult && !authResult.error) {
+        // Hide auth UI, then load client library.
+        authorizeDiv.style.display = 'none';
+        loadSheetsApi();
+    } else {
+        // Show auth UI, allowing the user to initiate authorization by
+        // clicking authorize button.
+        authorizeDiv.style.display = 'inline';
+    }
 }
+/*
 
-/**
- * Initiate auth flow in response to user clicking authorize button.
- *
- * @param {Event} event Button click event.
- */
-function handleAuthClick(event) {
-  gapi.auth.authorize(
-    {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
-    handleAuthResult);
-  return false;
-}
+gapi.client.sheets.spreadsheets.values.get({
+    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+    range: 'Class Data!A2:E',
+}).then(function(response) {
+    var range = response.result;
+
+    if (range.values.length > 0) {
+        appendPre('Name, Major:');
+        for (i = 0; i < range.values.length; i++) {
+            var row = range.values[i];
+            // Print columns A and E, which correspond to indices 0 and 4.
+            appendPre(row[0] + ', ' + row[4]);
+        }
+    } else {
+        appendPre('No data found.');
+    }
+}, function(response) {
+    appendPre('Error: ' + response.result.error.message);
+});
+*/
